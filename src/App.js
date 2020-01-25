a4c2b42fed07da153bf65ea25ca1469e669684b3
tabe <tabetabe.unity@gmail.com>
2020-01-25 23:28:40 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const server = "https://api.thecatapi.com/v1/images/search";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      result: 'https://cdn2.thecatapi.com/images/2kj.jpg',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    axios.get(server)
+      .then((res) => {
+        this.setState({
+          status: true,
+          result: res.data[0].url,
+        });
+        console.log(res);
+        console.log(res.data[0].url);
+      })
+      .catch((e) => {
+        console.log(e);
+        this.setState({
+          status: false,
+          result: e,
+        });
+      });
+    console.log('handleClick is called');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome nuko get page!!</h1>
+        <img src={this.state.result}></img>
+        <div>
+          <button onClick={this.handleClick}>Get nuko!</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
